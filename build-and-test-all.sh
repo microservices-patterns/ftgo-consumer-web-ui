5e2b94cedf6685cb1c85b9548b64d53704061bc5
Andrew Revinsky <andrew.revinsky@digitalarrowtech.com>
2021-04-22 23:07:33 +0300

    5 circle ci configuration (#6)
    * #5: Create CI configuration and a build script
    
    * #5: Create CI configuration and a build script
     - add master-branch filter
     - adjust loglevel for npm installation

---
+++ build-and-test-all.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -e
+
+echo "00. Environment diagnostics"
+
+npx envinfo
+
+
+echo ""
+echo ""
+echo "10. Ensuring required env variables and directories"
+
+if [[ -z "$JEST_JUNIT_OUTPUT_DIR_PARENT" ]]; then
+    export JEST_JUNIT_OUTPUT_DIR_PARENT=./reports/
+fi
+
+rm -rf "$JEST_JUNIT_OUTPUT_DIR_PARENT"
+mkdir -p "$JEST_JUNIT_OUTPUT_DIR_PARENT"
+
+if [[ -z "$JEST_JUNIT_OUTPUT_DIR" ]]; then
+    export JEST_JUNIT_OUTPUT_DIR=$JEST_JUNIT_OUTPUT_DIR_PARENT/junit/
+    mkdir -p "$JEST_JUNIT_OUTPUT_DIR"
+fi
+
+if [[ -z "$CI_ARTEFACTS_PATH" ]]; then
+    export CI_ARTEFACTS_PATH=$(pwd)/ci-artefacts/
+    mkdir -p "$CI_ARTEFACTS_PATH"
+fi
+mkdir -p "$CI_ARTEFACTS_PATH/npm-logs"
+
+
+echo ""
+echo ""
+echo "20. Installing dependencies"
+
+echo "running npm install in $(pwd)"
+rm -rf node_modules
+rm -f package-lock.json
+npm install --no-audit # --loglevel verbose
+npm audit fix
+
+
+echo ""
+echo ""
+echo "30. Running tests"
+
+npm run test
+
+
+echo ""
+echo ""
+echo "40. Building the site"
+
+npm run build
+
+
+echo ""
+echo ""
+echo "50. Archiving and copying the resulted built files"
+
+tar -czvf "$CI_ARTEFACTS_PATH/build_$(date '+%Y%m%d_%H%M').tar.gz" build
+
+cp ./junit.xml "$JEST_JUNIT_OUTPUT_DIR/junit_$(date '+%Y%m%d_%H%M').xml" 2>/dev/null || :
+
+echo "Copy NPM logs"
+[ -d "$HOME/.npm/_logs" ] && cp -R ~/.npm/_logs/* "$CI_ARTEFACTS_PATH/npm-logs" 2>/dev/null || :
