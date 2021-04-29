cf9077915ce9ddc8ecde01e7328e194f4c63f9bb
Andrew Revinsky (DART) <andrew.revinsky@digitalarrowtech.com>
2021-04-30 02:50:08 +0300

    #3: Implement a second page, Restaurants List  - a typo is removed

--- src/ui/pages/RestaurantListPage/index.js
+++ src/ui/pages/RestaurantListPage/index.js
@@ -14,7 +14,7 @@ import { LessLargeTextDiv, Span } from '../../elements/textElements';
 import { RoundedButton } from '../../elements/formElements';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-a
+
 export const RestaurantListPage = () => {
 
   const dispatch = useDispatch();
