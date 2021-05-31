d4970385fe7ea9acdcfa799e05015dc1110fa32e
Andrew Revinsky (DART) <andrew.revinsky@digitalarrowtech.com>
2021-05-31 21:32:50 +0300

    #17: Implement UI tests  - adding localization for time (for e2e tests)

--- tests-ui/testInfoProvider.js
+++ tests-ui/testInfoProvider.js
@@ -8,7 +8,7 @@ export function obtainTestInfo() {
   const result = {
     goodAddress: {
       address: `Testing Address: ${ faker.random.words(2) }`,
-      time: '10:58'
+      time: '10:58 PM'
     },
     email: getUniqueEmailAddressForTests()
     //phone: faker.phone.phoneNumber(),
