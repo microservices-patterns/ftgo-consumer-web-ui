fc77a17cced681dc3e8beed71b85a4306eedf0f5
Andrew Revinsky (DART) <andrew.revinsky@digitalarrowtech.com>
2021-07-13 04:16:18 +0300

    #24: Build 55, 56, and 57 failed  - Re-checking ports right after docker-compose up --build

--- start-http-server.sh
+++ start-http-server.sh
@@ -2,3 +2,7 @@
 
 docker-compose up -d --build
 #docker-compose exec web /bin/bash
+
+echo "Testing the ports of containers using their expected names:"
+docker port ftgoconsumerwebui_frontend_1
+docker port ftgoconsumerwebui_backend_1
